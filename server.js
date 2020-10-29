0150265dde6077040700ebfacbb4860360311b36
jbrown7407 <jbrown7407@gmail.com>
2020-10-29 13:39:11 -0400

    db fixed

--- server.js
+++ server.js
@@ -45,14 +45,15 @@ const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/'+ 'me
 // Connect to Mongo
 mongoose.connect(MONGODB_URI ,  { useNewUrlParser: true,  useUnifiedTopology: true});
 
+const db = mongoose.connection
 ////////
 // Error / success
-// db.on('error', (err) => console.log(err.message + ' is Mongod not running?'));
-// db.on('connected', () => console.log('mongo connected: ', MONGODB_URI));
-// db.on('disconnected', () => console.log('mongo disconnected'));
+db.on('error', (err) => console.log(err.message + ' is Mongod not running?'));
+db.on('connected', () => console.log('mongo connected: ', MONGODB_URI));
+db.on('disconnected', () => console.log('mongo disconnected'));
 
-// // open the connection to mongo
-// db.on('open' , ()=>{});
+// open the connection to mongo
+db.on('open' , ()=>{});
 ////////
 ////////
 
