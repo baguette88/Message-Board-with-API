8aee66cef69413eccea9ae928f29ba8a6cdffeb7
jbrown7407 <jbrown7407@gmail.com>
2020-10-31 01:47:42 -0400

    socket started again

--- server.js
+++ server.js
@@ -172,56 +172,56 @@ app.patch('/messages/:id', (req,res) => {
 // const app = express()
 
 
-//set the template engine ejs
-app.set('view engine', 'ejs')
+// //set the template engine ejs
+// app.set('view engine', 'ejs')
 
-//middlewares
-app.use(express.static('public'))
+// //middlewares
+// app.use(express.static('public'))
 
 
-//routes
-// app.get('/', (req, res) => {
-// 	res.render('index')
+// //routes
+// // app.get('/', (req, res) => {
+// // 	res.render('index')
+// // })
+
+// //Listen on port 3000
+// server = app.listen(3000)
+
+
+
+// //socket.io instantiation
+// const io = require("socket.io")(server)
+
+
+// //listen on every connection
+// io.on('connection', (socket) => {
+// 	console.log('New user connected')
+
+// 	//default username
+// 	socket.username = "Anonymous"
+
+//     //listen on change_username
+//     socket.on('change_username', (data) => {
+//         socket.username = data.username
+//     })
+
+//     //listen on new_message
+//     socket.on('new_message', (data) => {
+//         //broadcast the new message
+//         io.sockets.emit('new_message', {message : data.message, username : socket.username});
+//     })
+
+//     //listen on typing
+//     socket.on('typing', (data) => {
+//     	socket.broadcast.emit('typing', {username : socket.username})
+//     })
 // })
-
-//Listen on port 3000
-server = app.listen(3000)
-
-
-
-//socket.io instantiation
-const io = require("socket.io")(server)
-
-
-//listen on every connection
-io.on('connection', (socket) => {
-	console.log('New user connected')
-
-	//default username
-	socket.username = "Anonymous"
-
-    //listen on change_username
-    socket.on('change_username', (data) => {
-        socket.username = data.username
-    })
-
-    //listen on new_message
-    socket.on('new_message', (data) => {
-        //broadcast the new message
-        io.sockets.emit('new_message', {message : data.message, username : socket.username});
-    })
-
-    //listen on typing
-    socket.on('typing', (data) => {
-    	socket.broadcast.emit('typing', {username : socket.username})
-    })
-})
 
 
 
 
 
 // the app running the server    PORT SHOULD BE ON?!
-// app.listen(PORT, () => {
-//   console.log('listening')
-// })
+app.listen(PORT, () => {
+  console.log('listening')
+})
