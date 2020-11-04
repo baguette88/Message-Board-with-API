c6ccf91c81d6305bf96380e811ee4984e757dfb4
jbrown7407 <jbrown7407@gmail.com>
2020-11-04 10:37:33 -0500

    x
    xx

--- server.js
+++ server.js
@@ -7,7 +7,7 @@
 //npm install express-session
 //npm install dotenv
 
-const express = require('express')
+const express = require('express');
 const app = express()
 const mongoose = require('mongoose')
 const methodOverride = require('method-override')
